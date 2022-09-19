38e30968126048ae79f7d60d58fbe6a8f064e1e6
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2022-09-19 13:28:18 -0400

    V-menu APP

---
+++ src/components/FilipMenuComponent.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-menu
+    :offset-x="isOffsetX"
+    :offset-y="isOffsetY"
+    :open-on-hover="isOpenOnHover"
+    :transition="transition">
+        <template v-slot:activtor="{ on }">
+            <v-btn
+                v-if="icon"
+                :color="color"
+                v-on="on">
+                    <v-icon>
+                        {{ icon }}
+                    </v-icon>
+                </v-btn>
+                <v-list-item
+                    v-else-if="isSubMenu"
+                    class="d-flex justify-space-between"
+                    v-on="on">
+                        {{ name }}
+                        <v-icon>far fa-chevron-right</v-icon>
+                    </v-list-item>
+                    <v-btn
+                        v-else
+                        :color="color"
+                        v-on="on"
+                        text
+                        tile>
+                        {{ name }}
+                    </v-btn>
+        </template>
+        <v-list>
+            <template v-for="(item,index) in menuItems">
+                <v-divider v-if="item.isDivider" :key="index"/>
+                <filip-menu-component
+                    v-else-if="item.menu"
+                    :key="index"
+                    :name="name.item"
+                    :menu-items="item.menu"
+                    @filip-menu-click="emitClickEvent"
+                    :is-open-on-hover="false"
+                    :is-offset-x="true"
+                    :is-offset-y="false"
+                    :is-sub-menu="true"></filip-menu-component>
+                <v-list-item
+                    v-else
+                    :key="index"
+                    @click="emitClickEvent(item)">
+                        <v-list-item-title>
+                            {{ item.name }}
+                        </v-list-item-title>
+                </v-list-item>
+            </template>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+export default {
+  name: "FilipMenuComponent",
+  props: {
+    name: String,
+    icon: String,
+    menuItems: Array,
+    color: { type: String, default: "secondary" },
+    isOffsetX: { type: Boolean, default: false },
+    isOffsetY: { type: Boolean, default: true },
+    isOpenOnHover: { type: Boolean, default: false },
+    isSubMenu: { type: Boolean, default: false },
+    transition: { type: String, default: "scale-transition" },
+  },
+  methods: {
+    emitClickEvent(item) {
+      this.$emit("filip-menu-click", item);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/VMenuComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="text-center">
+    <filip-menu-component
+      :name="name"
+      :menuItems="fileMenuItems"
+      @filip-menu-click="onMenuItemClick"></filip-menu-component>
+  </div>
+</template>
+
+<script>
+import FilipMenuComponent from './FilipMenuComponent.vue';
+
+export default {
+  name: "VMenuComponent",
+  components: { FilipMenuComponent },
+  data: () => ({
+    name: 'FílipMenu',
+    fileMenuItems: [
+      {
+        name: "Menu Item 1",
+        action: () => {
+          console.log("menu-item-1");
+        },
+      },
+      { isDivider: true },
+      { name: "Menu Item 2" },
+      {
+        name: "Sub-menu 1",
+        menu: [
+          { name: "1.1" },
+          { name: "1.2" },
+          {
+            name: "Sub-menu 2",
+            menu: [
+              { name: "2.1" },
+              { name: "2.2" },
+              {
+                name: "Sub-menu 3",
+                menu: [
+                  { name: "3.1" },
+                  { name: "3.2" },
+                  {
+                    name: "Sub-menu 4",
+                    menu: [{ name: "4.1" }, { name: "4.2" }, { name: "4.3" }],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      { name: "Menu Item 3" },
+      { isDivider: true },
+      {
+        name: "Menu Item 4",
+        action: () => {
+          console.log("menu-item 4");
+        },
+      },
+      {
+        name: "Menu Item 5",
+        action: () => {
+          console.log("menu-item 5");
+        },
+      },
+    ],
+  }),
+  methods: {
+    onMenuItemClick(item) {
+      console.log(`onMenuItemClick(), item=${item}`);
+      if (item.action) {
+        item.action();
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
